9cf68e92f8593238bce466e1585e750936b2f031
takizawazaku <96064983+takizawazaku@users.noreply.github.com>
2022-02-06 15:54:28 +0900

    Pageの作成

--- src/App.js
+++ src/App.js
@@ -1,33 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/template/DefaultLayout";
-import { HeaderOnly } from "./components/template/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "滝沢",
-  image: "https://source.unsplash.com/yMSecCHsIBc",
-  email: "12345@example.com",
-  phone: "090-1111-2222",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout className="App">
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>テスト</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
--- src/components/atoms/layout/Header.jsx
+++ src/components/atoms/layout/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <SHeader>
       <SLink to="/">HOME</SLink>
-      <SLink to="/user">USER</SLink>
+      <SLink to="/users">USER</SLink>
     </SHeader>
   );
 };
