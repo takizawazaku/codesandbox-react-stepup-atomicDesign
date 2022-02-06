1267fb8dbe129ab5dbdea2b285c8df0ae5948495
takizawazaku <96064983+takizawazaku@users.noreply.github.com>
2022-02-06 14:52:31 +0900

    Organismの作成2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
 
+const user = {
+  name: "滝沢",
+  image: "https://source.unsplash.com/yMSecCHsIBc",
+  email: "12345@example.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
+
 export default function App() {
   return (
     <div className="App">
@@ -11,7 +22,7 @@ export default function App() {
       <SecondaryButton>テスト</SecondaryButton>
       <br />
       <SearchInput />
-      <UserCard />
+      <UserCard user={user} />
     </div>
   );
 }
